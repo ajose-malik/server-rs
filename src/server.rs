ec4d0f0f84c33bc11bb3702da9561607aadf8cbf
ajosemalik <ajose.malik@gmail.com>
2022-04-16 20:23:42 -0400

    add TCP connection

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::net::TcpListener;
+use std::io::Read;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +12,17 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+        
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                   let mut buffer = [0; 1024];
+                   stream.read(&mut buffer);
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e)
+            }
+        }
     }
 }
