0f703eafeedb69cbec8513b336800578f4157f2d
ajosemalik <ajose.malik@gmail.com>
2022-05-30 14:30:34 -0400

    implement custom trait for request handler

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,5 +1,5 @@
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::net::TcpStream;
+// use std::fmt::{Display, Formatter, Result as FmtResult};
+// use std::net::TcpStream;
 use std::io::{Write, Result as IoResult};
 
 use super::StatusCode;
@@ -15,7 +15,7 @@ impl Response {
     Response { status_code, body }
   }
 
-  pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+  pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
     let body = match &self.body {
       Some(b) => b,
       None => "",
@@ -23,23 +23,6 @@ impl Response {
 
     write!(
       stream,
-      "HTTP/1.1 {} {}\r\n\r\n{}",
-      self.status_code,
-      self.status_code.reason_phrase(),
-      body,
-    )
-  }
-}
-
-impl Display for Response {
-  fn fmt(&self, f: &mut Formatter) -> FmtResult {
-    let body = match &self.body {
-      Some(b) => b,
-      None => "",
-    };
-
-    write!(
-      f,
       "HTTP/1.1 {} {}\r\n\r\n{}",
       self.status_code,
       self.status_code.reason_phrase(),
